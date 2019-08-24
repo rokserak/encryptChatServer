44e51f8e5de5a977ac82f41b3b4fe6853a3038d8
rokserak <rok.serak@gmail.com>
2019-08-24 15:41:35 +0200

    init commit

---
+++ src/main/kotlin/com/rokserak/encryptChat/EncryptChatApplication.kt
@@ -0,0 +1,11 @@
+package com.rokserak.encryptChat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EncryptChatApplication
+
+fun main(args: Array<String>) {
+	runApplication<EncryptChatApplication>(*args)
+}
---
+++ src/test/kotlin/com/rokserak/encryptChat/EncryptChatApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rokserak.encryptChat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EncryptChatApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
